f88f24e6fea7ae3c593b458acf0bab3918c94862
CalebSReed <csereed@gmail.com>
2023-12-06 03:49:01 -0800

    HomeArrow Rotates correctly LETS GO!!!

--- Assets/Scripts/UI/HomeArrow.cs
+++ Assets/Scripts/UI/HomeArrow.cs
@@ -17,23 +17,12 @@ public class HomeArrow : MonoBehaviour
     {
         if (beaconLocation != null)
         {
-            /*Vector3 thisPos = transform.position;
-            thisPos.z = 10;
-            Camera.main.ScreenToWorldPoint(thisPos);
-            empty.position = thisPos;
-            Vector3 _look = empty.InverseTransformPoint(beaconLocation);//this is dumb that im using an empty transform idfk how else to do this bruh
-            float _angle = Mathf.Atan2(_look.y, _look.x) * Mathf.Rad2Deg;
-            sprTrans.rotation = Quaternion.Euler(new Vector3(0, 0, _angle));*/
-
-            Ray ray = Camera.main.ScreenPointToRay(empty.transform.position);
-            RaycastHit rayHit;
-            Physics.Raycast(ray, out rayHit);
-
-            var newPos = rayHit.point;
-            newPos.y = 0;
-            empty.position = newPos;
-            empty.LookAt(beaconLocation);
+            var newLook = beaconLocation.position;
+            newLook.y = 0;
+            empty.LookAt(newLook);
             //sprTrans.transform.eulerAngles = new Vector3(0, 0, empty.rotation.eulerAngles.y);//this is so dumb lol
+            //sprTrans.eulerAngles = new Vector3(0, 0, empty.eulerAngles.y);
+            empty.eulerAngles = new Vector3(0, 0, -empty.eulerAngles.y);
             sprTrans.rotation = empty.GetChild(0).rotation;
         }
         else
